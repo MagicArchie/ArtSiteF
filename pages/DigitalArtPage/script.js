ba2b12b4966f1b8471dd36add1cc4010750a0378
Archie <149274597+MagicArchie@users.noreply.github.com>
2024-02-08 02:38:20 +0200

    Update script.js

--- pages/DigitalArtPage/script.js
+++ pages/DigitalArtPage/script.js
@@ -13,8 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         'images/IMG12.jpg',
         'images/IMG13.jpg',
         'images/IMG14.jpg',
-        'images/IMG15.jpg',
-		'images/IMG16.jpg'
+        'images/IMG15.jpg'
     ];
 
     const slideshowContainer = document.getElementById('slideshow-container');
@@ -45,12 +44,12 @@ document.addEventListener('DOMContentLoaded', function () {
     showSlide(currentIndex);
 
     function nextSlide() {
-        currentIndex = (currentIndex + 1) % images.length;
+        currentIndex = (currentIndex + 1) % slides.length;
         showSlide(currentIndex);
     }
 
     function prevSlide() {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         showSlide(currentIndex);
     }
 
